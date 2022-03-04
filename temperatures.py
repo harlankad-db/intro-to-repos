bcb5ed304d04b23fa46eba0d70e3cb1d251e52c1
harlankad-db <harlankad-db>
2022-03-04 16:07:51 +0000

    test commit

--- temperatures.py
+++ temperatures.py
@@ -52,3 +52,15 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md This is markdown!
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+
